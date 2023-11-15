13cc329262d10bbc9b35d6aa83e2749e6b5bd6ae
henrique <henrique.pim2@gmail.com>
2023-11-15 14:31:59 -0300

    criação do função comoDicionário Retorna as informações do usuário como dicionário.

--- apiLoja/userManager.py
+++ apiLoja/userManager.py
@@ -10,6 +10,17 @@ class User:
         self.telefone = telefone
         self.email = email
         self.validador = validador
+    
+    def comoDicionario(self):
+        return {
+            'idUsuario': self.idUsuario,
+            'nome': self.Nome,
+            'cpf': self.cpf,
+            'dataNascimento': self.DataNascimento,
+            'telefone': self.telefone,
+            'email': self.email,
+            'validor': self.validador
+        }
 
 def validator(email: str, password: str, key: str):
     return sha256(email.encode() + password.encode() + key.encode()).hexdigest()
