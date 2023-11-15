90406ec3f6c9cb75d0ef23d20a6134e6d52c5f7a
ESADDUM <eduaddum@gmail.com>
2023-11-15 14:19:39 -0300

    class UserManeger

--- apiLoja/userManager.py
+++ apiLoja/userManager.py
@@ -1,4 +1,8 @@
-from hashlib import sha256
+from userManager import UserManager
+class usuer:
+    def __init__(self, idUsuario,Nome, cpf, DataNascimento, telefone, email, validador):
+        self.idUsuario = idUsuario
+        self.Nome = Nome
 
 def validator(email: str, password: str, key: str):
     return sha256(email.encode() + password.encode() + key.encode()).hexdigest()
