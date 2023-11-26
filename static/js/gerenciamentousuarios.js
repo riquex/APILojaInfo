73670f4fa420db6c726342a7a6b3d5f600c6ab2a
henrique <henrique.pim2@gmail.com>
2023-11-26 14:54:08 -0300

    ordenamento reverso

--- static/js/gerenciamentousuarios.js
+++ static/js/gerenciamentousuarios.js
@@ -12,7 +12,7 @@ window.onload = () => {
     const complemento = document.getElementById("complemento");
     const clickEvent = new Event("click");
 
-    let orberby = ['id', 'asc', (a, b) => 0]
+    let orberby = ['idUsuario', false, (a, b) => 0]
 
     const numSort = (a, b) => a[orberby[0]] - b[orberby[0]];
     const strSort = (a, b) => {
@@ -28,58 +28,69 @@ window.onload = () => {
     };
 
     id.onclick = () => {
+        orberby[2] = orberby[0] == "idUsuario" ? !orberby[2]: orberby[2];
         orberby[0] = "idUsuario";
         orberby[3] = numSort;
         update.dispatchEvent(clickEvent);
     };
 
     nome.onclick = () => {
+        orberby[2] = orberby[0] == "nome" ? !orberby[2]: orberby[2];
         orberby[0] = "nome";
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
 
     nascimento.onclick = () => {
+        orberby[2] = orberby[0] == "dataNascimento" ? !orberby[2]: orberby[2];
         orberby[0] = "dataNascimento"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
     telefone.onclick = () => {
+        orberby[2] = orberby[0] == "telefone" ? !orberby[2]: orberby[2];
         orberby[0] = "telefone"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
     cpf.onclick = () => {
+        orberby[2] = orberby[0] == "cpf" ? !orberby[2]: orberby[2];
         orberby[0] = "cpf"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
     email.onclick = () => {
+        orberby[2] = orberby[0] == "email" ? !orberby[2]: orberby[2];
         orberby[0] = "email"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
     cep.onclick = () => {
+        orberby[2] = orberby[0] == "cep" ? !orberby[2]: orberby[2];
         orberby[0] = "cep"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
     rua.onclick = () => {
+        orberby[2] = orberby[0] == "rua" ? !orberby[2]: orberby[2];
         orberby[0] = "rua"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
     municipio.onclick = () => {
+        orberby[2] = orberby[0] == "municipio" ? !orberby[2]: orberby[2];
         orberby[0] = "municipio"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
     estado.onclick = () => {
+        orberby[2] = orberby[0] == "estado" ? !orberby[2]: orberby[2];
         orberby[0] = "estado"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
     };
     complemento.onclick = () => {
+        orberby[2] = orberby[0] == "complemento" ? !orberby[2]: orberby[2];
         orberby[0] = "complemento"
         orberby[3] = strSort;
         update.dispatchEvent(clickEvent);
@@ -110,7 +121,8 @@ window.onload = () => {
             if (data instanceof Array){
 
                 data.sort(orberby[3])
-                console.log(data);
+                if (orberby[2])
+                    data.reverse();
 
                 for(let i = 0; i < data.length; ++i){
                     let row = table.insertRow();
